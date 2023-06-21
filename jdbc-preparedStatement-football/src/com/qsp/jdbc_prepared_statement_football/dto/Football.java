13c2b11313f050f3fcd0ec912e77b61c03119c48
Arpit Brur <126546064+Arpitbrur@users.noreply.github.com>
2023-06-21 09:48:27 +0530

    Adding: JDBC project prepared Statement football

---
+++ jdbc-preparedStatement-football/src/com/qsp/jdbc_prepared_statement_football/dto/Football.java
@@ -0,0 +1,36 @@
+package com.qsp.jdbc_prepared_statement_football.dto;
+
+public class Football {
+  
+	private int id;
+	private String color;
+	private String brand;
+	private Double price;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	public String getBrand() {
+		return brand;
+	}
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+	public Double getPrice() {
+		return price;
+	}
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+	
+	
+}
